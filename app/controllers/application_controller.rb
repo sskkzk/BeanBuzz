719e8f22cf89118289facfea7797880fab300265
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-06-16 10:46:15 +0000

    アフターログアウトパス修正２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    homes_about_path # サインアウト後にhomesのaboutページにリダイレクト
+    root_path # サインアウト後にhomesのtopページにリダイレクト
   end
 
   protected
