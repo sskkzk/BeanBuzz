d047b2ef77f3429828360688344806f805be5dd8
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-07-04 12:18:54 +0000

    残りレスポンシブ

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,6 +8,6 @@ environment.plugins.prepend(
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
+    Popper: ['popper.js', 'default'] // ここを修正
   })
 )
