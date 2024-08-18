4ecaf4e5509f853819fc2b188454e9702170ad36
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-08-18 08:35:16 +0000

    いいね修正3

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -6,16 +6,16 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     @favorite = current_user.favorites.build(post: @post)
     if @favorite.save
-      redirect_to @post, notice: 'お気に入りに追加しました。'
+      redirect_to @post, notice: 'いいねしました。'
     else
-      redirect_to @post, alert: 'お気に入りの追加に失敗しました。'
+      redirect_to @post, alert: 'いいねに失敗しました。'
     end
   end
 
   def destroy
     @favorite = current_user.favorites.find_by(post_id: params[:post_id])
     @favorite.destroy
-    redirect_to @favorite.post, notice: 'お気に入りを解除しました。'
+    redirect_to @favorite.post, notice: 'いいねを解除しました。'
   end
   
 end
