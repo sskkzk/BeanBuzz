b4e03a8c8092fe78de848b8f1ba313e7c7358f2f
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-08-19 12:19:16 +0000

    修正最終かな

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,4 @@
 class Public::HomesController < ApplicationController
-  # before_action :authenticate_user!, if: :require_authentication?, except: [:about, :top]
 
   def top
   end
@@ -7,10 +6,4 @@ class Public::HomesController < ApplicationController
   def about
   end
 
-  # private
-
-  # def require_authentication?
-  #   # without_authentication パラメータが true の場合、認証をスキップする
-  #   params[:without_authentication] != true
-  # end
 end
