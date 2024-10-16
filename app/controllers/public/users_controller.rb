781edd4feac118738913f622fa3f389905e4dc55
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-10-16 13:36:39 +0000

    usersコントローラー短縮

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,17 +9,14 @@ class Public::UsersController < ApplicationController
   end
   
   def following
-    @user = User.find(params[:id])
     @followees = @user.followees.page(params[:page]).per(10)  # ページネーションの追加
   end
 
   def followers
-    @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page]).per(10)  # ページネーションの追加
   end
 
   def favorites
-    @user = User.find(params[:id])
     @favorites = @user.favorites.includes(:post).page(params[:page]).per(10)  # お気に入りの投稿を取得し、ページネーションを追加
   end
   
@@ -34,33 +31,27 @@ class Public::UsersController < ApplicationController
 
   # GET /users/:id
   def show
-  @user = User.find(params[:id])
-  
-    # 現在のユーザーが対象のユーザーの場合はマイページへリダイレクト
     if current_user == @user
       redirect_to mypage_path
-      return # ここで処理を終了させる
+      return
     end
   
-    # 鍵垢関係の処理をスキップするための追加チェック
     if @user.is_private && !current_user.following?(@user)
       redirect_to root_path, alert: "このユーザーのプロフィールは非公開です。"
-      return # ここで処理を終了させる
+      return
     end
 
-  @posts = @user.posts.page(params[:page]).per(10)
-  @comments = @user.comments.page(params[:page]).per(10)
+    @posts = @user.posts.page(params[:page]).per(10)
+    @comments = @user.comments.page(params[:page]).per(10)
   end
 
   # GET /users/:id/edit
   def edit
-    @user = current_user
     redirect_to root_path, alert: 'Access denied.' unless @user == current_user
   end
 
   # PATCH /users/:id
   def update
-  @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(current_user), notice: '設定が更新されました。'
     else
@@ -85,6 +76,5 @@ class Public::UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :user_image, :is_private)
   end
-  
-
 end
+
